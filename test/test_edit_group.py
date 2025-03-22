38ffe4fd213d685512dccca9570af376b31dbd4e
olgazhadaeva <olga.zhadaeva14@gmail.com>
2025-03-23 01:13:47 +0300

    Добавлен переход на страницу контактов в методах удаления, модификации контакта, добавлено переиспользование кода

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,6 +1,16 @@
 from model.group import Group
 
-def test_edit_first_group(app):
+def test_edit_first_group_full(app):
     app.session.login("admin", "secret")
     app.group.edit_first_group(Group(name="1test", header="2test", footer="12test"))
+    app.session.logout()
+
+def test_edit_first_group_name(app):
+    app.session.login("admin", "secret")
+    app.group.edit_first_group(Group(name="New group"))
+    app.session.logout()
+
+def test_edit_first_group_header(app):
+    app.session.login("admin", "secret")
+    app.group.edit_first_group(Group(header="New header"))
     app.session.logout()
