5f0142a1cd67e9767a7b31924e2abceddace29bc
olgazhadaeva <olga.zhadaeva14@gmail.com>
2025-03-23 15:26:32 +0300

    Реализованы проверки валидности фикстуры и состояния сессии

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,8 +2,6 @@
 from model.contact import Contact
 
 def test_add_contact(app):
-    app.session.login("admin", "secret")
     app.contact.create(Contact(firstname="Ivan", middlename="Ivanovich", lastname="Ivanov", nickname="Vanya", title="Man", company="unreal", address="Moscow", home="St. Petersburg",
                             mobile="88888888888", work="89888888888", fax="89988888888", email="Ivan@gmail.com", email2="Ivan@mail.ru",
                             email3="Iban@yandex.ru", homepage="something", bday="1", bmonth="January", byear="2000", aday="2", amonth="February", ayear="2000"))
-    app.session.logout()
